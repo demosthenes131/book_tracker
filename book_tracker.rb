53cb86b79ac30ae4adde0dbebd81f2a80d18bb9b
Nick Queen <nicolasqueen@gmail.com>
2015-06-30 03:27:49 +0000

    Updates book_tracker.rb

--- book_tracker.rb
+++ book_tracker.rb
@@ -1,8 +1,32 @@
 book_list = []
+review_list = []
+hash = { "book" => [book_list], "review" => [review_list] }
 
 loop do
-puts "Add a book! "
-book = $stdin.gets.chomp
-book_list.push(book)
-puts book_list.inspect
+  puts "Do you want to add a book? (yes or no)"
+  input = gets.chomp.downcase
+    case input
+      when 'yes'
+        puts "Add a book! "
+        book = gets.chomp
+        book_list.push(book)
+        puts "How about a short, one sentence review? (yes or no)"
+          input = gets.chomp.downcase
+            case input
+            when 'yes'
+              puts "Add your review!"
+              review = gets.chomp
+              review_list.push(review)
+            when 'no'
+            end
+      when 'no'
+        puts book_list.inspect
+        puts hash.inspect
+        hash.each do |book_hash, review_hash|
+          puts "#{book_hash} - #{review_hash}"
+        end  
+        break
+      else
+        break
+    end  
 end
